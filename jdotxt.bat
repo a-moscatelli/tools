5181ad545661a750bcc2b4b2e7459f2338491f0b
a-moscatelli <a.moscatelli@gmail.com>
2025-03-18 11:32:28 +0100

    Add files via upload

---
+++ jdotxt.bat
@@ -0,0 +1,7 @@
+
+set JAR=C:\ProgramData\AppV\609D7BCF-0413-4631-9FE9-A38C2108E499\03AB7A83-562B-418B-82CF-5E3C01B32276\Root\runtime\bin\java.exe
+set LOCL=C:\Users\moscaal\AMLOCAL\
+cd /d %LOCL%\
+
+%JAR% -jar jdotxt-1.8-java8.jar
+
