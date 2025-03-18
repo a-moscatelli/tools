9095a1091c4b37d0e06747a5cc44d9986ab7c603
a-moscatelli <a.moscatelli@gmail.com>
2025-03-18 11:33:52 +0100

    Add files via upload

---
+++ javarunner.bat
@@ -0,0 +1,3 @@
+cd /d C:\Users\moscaal\AMLOCAL\apps
+set JAVA_HOME=C:\ProgramData\AppV\609D7BCF-0413-4631-9FE9-A38C2108E499\03AB7A83-562B-418B-82CF-5E3C01B32276\Root\runtime
+%JAVA_HOME%\bin\java -jar %1
